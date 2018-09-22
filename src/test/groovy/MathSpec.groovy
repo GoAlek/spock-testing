d0cf3f685642afc4d0641b56de974c162ded6e2f
ago <alek.golaszewski@gmail.com>
2018-09-22 18:45:53 +0200

    Very first commit

---
+++ src/test/groovy/MathSpec.groovy
@@ -0,0 +1,17 @@
+import spock.lang.Specification
+import spock.lang.Unroll
+
+class MathSpec extends Specification {
+
+	@Unroll
+	def "maximum of two numbers"() {
+		expect:
+			Math.max(a, b) == c
+
+		where:
+			a | b || c
+			1 | 3 || 3
+			7 | 4 || 7
+			0 | 0 || 0
+	}
+}
